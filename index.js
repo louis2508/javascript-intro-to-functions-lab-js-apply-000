5628de9352b60e33af47c3683e09d71c389849e3
louis portalatin <l.portalatin181@gmail.com>
2017-08-29 20:10:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function whisper(string) {
+  return string.toLowerCase()
+}
+function shout(string) {
+  return string.toUpperCase()
+}
+function logShout(string) {
+console.log(string.toUpperCase()
+)
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase()
+)
+}
+function sayHiToGrandma.toUpperCase()
